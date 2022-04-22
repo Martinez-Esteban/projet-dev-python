de3f257b76761c4818f5daafe7e2b29c4bd53303
Esteban Martinez <esteban.martinez@ynov.com>
2022-04-22 12:37:57 +0200

    socket + threading

--- client.py
+++ client.py
@@ -1,9 +1,13 @@
 import socket
 
-connexion_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connexion_serveur.connect(('localhost', 15556))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 1111))
 
-response = connexion_serveur.recv(1024)
-print(response.decode())
-
-connexion_serveur.close()
+print("Le nom du fichier que vous voulez récupérer:")
+file_name = input(">> ") # utilisez raw_input() pour les anciennes versions python
+s.send(file_name.encode())
+file_name = 'data/%s' % (file_name,)
+r = s.recv(9999999)
+with open(file_name,'wb') as _file:
+    _file.write(r)
+print("Le fichier a été correctement copié dans : %s." % file_name)
--- server.py
+++ server.py
@@ -1,10 +1,33 @@
-import socket
+import socket, threading
 
-socket_ecoute = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket_ecoute.bind(('', 15556))
-socket_ecoute.listen()
+class ClientThread(threading.Thread):
 
-connexion_client, adresse_client = socket_ecoute.accept()
+    def __init__(self, ip, port, clientsocket):
 
-connexion_client.sendall(b'Hey my name is Olivier!')
-socket_ecoute.close()
+        threading.Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        self.clientsocket = clientsocket
+        print("[+] Nouveau thread pour %s:%s" % (self.ip, self.port, ))
+
+    def run(self): 
+   
+        print("Connexion de %s:%s" % (self.ip, self.port, ))
+
+        r = self.clientsocket.recv(2048)
+        print("Ouverture du fichier: ", r, "...")
+        fp = open(r, 'rb')
+        self.clientsocket.send(fp.read())
+
+        print("Client déconnecté...")
+
+tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+tcpsock.bind(("",1111))
+
+while True:
+    tcpsock.listen(10)
+    print( "En écoute...")
+    (clientsocket, (ip, port)) = tcpsock.accept()
+    newthread = ClientThread(ip, port, clientsocket)
+    newthread.start()
